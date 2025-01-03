f98729627d63a116facdc70928afd4ac4e5cfeab
SaiWinMaung <saiwinmaung.mc@gmail.com>
2025-01-03 13:12:28 +0630

    table column name

--- chart.py
+++ chart.py
@@ -1,4 +1,24 @@
 import streamlit as st
+import pandas as pd
 
-def ChartView ():
-    st.title('Chart Page')
+def ChartView():
+    df = pd.read_csv('summer_movies.csv')
+    df =df.dropna()
+    image, header = st.columns([0.2, 0.8])
+    with image:
+        st.image("movie.jpg", width=150,)    
+    html_title = """
+    <style>
+    .title-test{
+    font-weight:bold;
+    padding: 5px;
+    border-radius:6px
+    }
+    </style>
+    <center><h1 class = "title-test"> IMDB Summer Festival Dashboard </h1></center>
+    """
+    with header:
+        st.markdown(html_title, unsafe_allow_html=True)
+
+    st.sidebar.radio('Select Title type', options= df['title_type'].unique())
+    
--- table.py
+++ table.py
@@ -1,10 +1,35 @@
 import streamlit  as st
 import pandas as pd
 
-
-df = pd.read_csv('summer_movies.csv')
-df.dropna(inplace= True)
-print(df.isnull().sum())
 def TableView() :
-    st.title('IMDB Summer Festival Data Table')
-    st.write(df)
+    df = pd.read_csv('summer_movies.csv')
+    df = df.dropna()
+    html_title = """
+    <style>
+    .title-test{
+    font-weight:bold;
+    padding: 5px;
+    border-radius:6px
+    }
+    </style>
+    <center><h1 class = "title-test"> IMDB Summer Festival Dashboard </h1></center>
+    """
+    st.markdown(html_title, unsafe_allow_html=True)
+    st.dataframe(df)
+    num_row = df.shape[0]
+    row, rownum = st.columns(2)
+    with row:
+        st.markdown('***Total Number of Row*** :')
+
+    with rownum:
+        st.text(num_row)
+
+    st.write('')
+    col, colname,  = st.columns(2)
+    with col:
+        st.markdown('***Column Name*** :')
+    with colname:
+        for col in df.columns:
+            st.text(col)
+
+   
