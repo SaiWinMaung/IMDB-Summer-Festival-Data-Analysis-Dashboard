fe4d0ed735ce94b78ceb18bc85f8e22c547aa0aa
SaiWinMaung <saiwinmaung.mc@gmail.com>
2025-01-09 14:01:25 +0630

    table with bar chart

--- chart.py
+++ chart.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from io import BytesIO
 
 
 def ChartView():
@@ -49,26 +52,85 @@ def ChartView():
             )
 
         if type == "movie":
-            Card('Total Number of Movie')
+            Card("Total Number of Movie")
         elif type == "film":
-            Card('Total Number of Film')
+            Card("Total Number of Film")
         else:
-            Card('Total Number of Video')
-
+            Card("Total Number of Video")
 
     st.divider()
-    selected_columns = ['primary_title','runtime_minutes']
+    selected_columns = ["primary_title", "runtime_minutes"]
     result = df_selection[selected_columns]
-    max_or_min = st.radio('Please choose Maximum or Minimum runtime', ['max', 'min'], horizontal= True)
+    max_or_min = st.radio(
+        "Please choose Maximum or Minimum runtime", ["max", "min"], horizontal=True
+    )
 
-    if max_or_min == 'max':
+    if max_or_min == "max":
+
+        def render_mini_bar_chart(value, max_value, width=80, height=5):
+            fig, ax = plt.subplots(figsize=(width / 80, height / 80))
+            ax.barh([0], [value], color="blue", height=0.8)
+            ax.set_xlim(0, max_value)
+            ax.axis("off")
+            buffer = BytesIO()
+            plt.savefig(buffer, format="png", bbox_inches="tight", pad_inches=0)
+            plt.close(fig)
+            buffer.seek(0)
+            return buffer
+
         sorted_df = result.sort_values(by = 'runtime_minutes', ascending= False)
-        sorted_df.reset_index(drop= True, inplace= True)
-        sorted_df.index = sorted_df.index + 1
-        st.write(sorted_df.head(10))
 
-    else :
-        sorted_df = result.sort_values(by = 'runtime_minutes',)
-        sorted_df.reset_index(drop= True, inplace= True)
-        sorted_df.index = sorted_df.index + 1
-        st.write(sorted_df.head(10))
+        top_10_df = sorted_df.head(10)
+        max_value = top_10_df['runtime_minutes'].max()
+
+        title1, title2, title3 =st.columns([2, 1, 3])
+        title1.markdown(f'''
+                <h3 style =" text-align: center;"> Title</h3>''', unsafe_allow_html= True)
+        title2.markdown(f'''
+                <h3 style =" text-align: center;"> Minutes</h3>''', unsafe_allow_html= True)
+        title3.markdown(f'''
+                <h3 style =" text-align: center;"> Bar Chart</h3>''', unsafe_allow_html= True)
+        for index, row in top_10_df.iterrows():
+            text = row["primary_title"]
+            value = row["runtime_minutes"]
+            bar_chart = render_mini_bar_chart(value, max_value)
+            
+            # Render the row with text, value, and bar chart
+            col1, col2, col3 = st.columns([2, 1, 2])
+            col1.write(text)
+            col2.write(value)
+            col3.image(bar_chart, use_column_width=True)
+
+    else:
+        def render_mini_bar_chart(value, max_value, width=80, height=5):
+            fig, ax = plt.subplots(figsize=(width / 80, height / 80))
+            ax.barh([0], [value], color="blue", height=0.8)
+            ax.set_xlim(0, max_value)
+            ax.axis("off")
+            buffer = BytesIO()
+            plt.savefig(buffer, format="png", bbox_inches="tight", pad_inches=0)
+            plt.close(fig)
+            buffer.seek(0)
+            return buffer
+        sorted_df = result.sort_values(
+            by="runtime_minutes",
+        )
+        top_10_df = sorted_df.head(10)
+        max_value = top_10_df['runtime_minutes'].max()
+        title1, title2, title3 =st.columns([2, 1, 3])
+        title1.markdown(f'''
+                <h3 style =" text-align: center;"> Title</h3>''', unsafe_allow_html= True)
+        title2.markdown(f'''
+                <h3 style =" text-align: center;"> Minutes</h3>''', unsafe_allow_html= True)
+        title3.markdown(f'''
+                <h3 style =" text-align: center;"> Bar Chart</h3>''', unsafe_allow_html= True)
+        for index, row in top_10_df.iterrows():
+            text = row["primary_title"]
+            value = row["runtime_minutes"]
+            bar_chart = render_mini_bar_chart(value, max_value)
+            
+            # Render the row with text, value, and bar chart
+            col1, col2, col3 = st.columns([2, 1, 2])
+            col1.write(text)
+            col2.write(value)
+            col3.image(bar_chart, use_column_width=True)
