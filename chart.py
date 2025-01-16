bf003aea9dafda3751fc7752f47d16c1e826d66a
SaiWinMaung <saiwinmaung.mc@gmail.com>
2025-01-16 19:04:44 +0630

    solve error

--- chart.py
+++ chart.py
@@ -162,7 +162,7 @@ def ChartView():
                 <p style = "font-color : red; ">{value}</p>""",
                 unsafe_allow_html=True,
             )
-            col3.image(bar_chart, use_column_width=True)
+            col3.image(bar_chart, use_container_width=True)
 
     # for unique genre
     st.divider()
